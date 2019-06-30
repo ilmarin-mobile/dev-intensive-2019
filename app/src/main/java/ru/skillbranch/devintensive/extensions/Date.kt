c0c1b4e2ff03ae3d32260085d5ea04737b19cc1f
ilmarin-mobile <marina.malinkina@gmail.com>
2019-06-30 22:47:22 +0300

    task - *Date.humanizeDiff

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
@@ -3,10 +3,24 @@ package ru.skillbranch.devintensive.extensions
 import java.text.SimpleDateFormat
 import java.util.*
 
-const val SECOND = 1000L
-const val MINUTE = 60 * SECOND
-const val HOUR = 60 * MINUTE
-const val DAY = 24 * HOUR
+enum class TimeUnits(
+    val value: Long
+) {
+    SECOND(1000L),
+    MINUTE(60 * SECOND.value),
+    HOUR(60 * MINUTE.value),
+    DAY(24 * HOUR.value);
+
+    companion object {
+        fun getTimeValue(value: Int, units: TimeUnits): Long =
+            when(units) {
+                SECOND -> value * SECOND.value
+                MINUTE -> value * MINUTE.value
+                HOUR -> value * HOUR.value
+                DAY -> value * DAY.value
+            }
+    }
+}
 
 fun Date.format(pattern: String = "HH:mm:ss dd.MM.yy"): String {
     val dateFormat = SimpleDateFormat(pattern, Locale("ru"))
@@ -17,21 +31,81 @@ fun Date.add(value: Int, units: TimeUnits = TimeUnits.SECOND): Date {
 
     var time = this.time
 
-    time += when(units) {
-        TimeUnits.SECOND -> value * SECOND
-        TimeUnits.MINUTE -> value * MINUTE
-        TimeUnits.HOUR -> value * HOUR
-        TimeUnits.DAY -> value * DAY
-    }
+    time += TimeUnits.getTimeValue(value, units)
 
     this.time = time
 
     return this
 }
 
-enum class TimeUnits {
-    SECOND,
-    MINUTE,
-    HOUR,
-    DAY
+fun Date.humanizeDiff(date: Date = Date()): String {
+    var timediff = this.time - date.time
+    val pastTime = timediff < 0
+    if (timediff < 0) {
+        timediff *= -1
+    }
+
+    return when (timediff) {
+        in 0 .. 1 * TimeUnits.SECOND.value  -> "только что"
+        in 1 * TimeUnits.SECOND.value .. 45 * TimeUnits.SECOND.value-1 -> "несколько секунд".pastOrFuture(pastTime)
+        in 45 * TimeUnits.SECOND.value .. 75 * TimeUnits.SECOND.value-1 -> "минуту".pastOrFuture(pastTime)
+        in 75 * TimeUnits.SECOND.value .. 45 * TimeUnits.MINUTE.value-1 -> "${getTimeDiffString(timediff, TimeUnits.MINUTE).pastOrFuture(pastTime)}"
+        in 45 * TimeUnits.MINUTE.value .. 75 * TimeUnits.MINUTE.value-1 -> "час".pastOrFuture(pastTime)
+        in 75 * TimeUnits.MINUTE.value .. 22 * TimeUnits.HOUR.value-1 -> "${getTimeDiffString(timediff, TimeUnits.HOUR).pastOrFuture(pastTime)}"
+        in 22 * TimeUnits.HOUR.value .. 26 * TimeUnits.HOUR.value-1 -> "день".pastOrFuture(pastTime)
+        in 26 * TimeUnits.HOUR.value .. 360 * TimeUnits.DAY.value -> "${getTimeDiffString(timediff, TimeUnits.DAY).pastOrFuture(pastTime)}"
+        else -> if (pastTime) "более года назад" else "более чем через год"
+    }
 }
+
+private fun String.pastOrFuture(pastTime: Boolean): String =
+    if (pastTime) "${this} назад" else "через ${this}"
+
+
+private fun getTimeDiffString(timediff: Long, timeUnit: TimeUnits): String =
+    when (timeUnit) {
+        TimeUnits.MINUTE -> "${getTimeUnitCount(timediff, TimeUnits.MINUTE)} ${getMinutesString(getTimeUnitCount(timediff, TimeUnits.MINUTE))}"
+        TimeUnits.HOUR -> "${getTimeUnitCount(timediff, TimeUnits.HOUR)} ${getHoursString(getTimeUnitCount(timediff, TimeUnits.HOUR))}"
+        TimeUnits.DAY -> "${getTimeUnitCount(timediff, TimeUnits.DAY)} ${getDaysString(getTimeUnitCount(timediff, TimeUnits.DAY))}"
+        else -> ""
+    }
+
+
+private fun getTimeUnitCount(timediff: Long, timeUnit: TimeUnits): Int =
+    ((timediff)/timeUnit.value).toInt()
+
+
+private fun getMinutesString(time: Int): String =
+    if (time in 11 .. 14) "минут"
+    else when(time%10) {
+        1 -> "минуту"
+        in 2 .. 4 -> "минуты"
+        else -> "минут"
+    }
+
+private fun getHoursString(time: Int): String =
+    if (time in 11 .. 14) "часов"
+    else when(time%10) {
+        1 -> "час"
+        in 2 .. 4 -> "часа"
+        else -> "часов"
+    }
+
+private fun getDaysString(time: Int): String =
+    if (time in 11 .. 14) "дней"
+    else when(time%10) {
+        1 -> "день"
+        in 2 .. 4 -> "дня"
+        else -> "дней"
+    }
+
+//0с - 1с "только что"
+//1с - 45с "несколько секунд назад"
+//45с - 75с "минуту назад"
+//75с - 45мин "N минут назад"
+//45мин - 75мин "час назад"
+//75мин 22ч "N часов назад"
+//22ч - 26ч "день назад"
+//26ч - 360д "N дней назад"
+//>360д "более года назад"
+
--- app/src/main/java/ru/skillbranch/devintensive/models/TextMessage.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/TextMessage.kt
@@ -1,5 +1,6 @@
 package ru.skillbranch.devintensive.models
 
+import ru.skillbranch.devintensive.extensions.humanizeDiff
 import java.util.*
 
 class TextMessage(
@@ -12,5 +13,5 @@ class TextMessage(
 ) : BaseMessage(id, from, chat, isIncoming, date) {
 
     override fun formatMessage(): String = "id:$id ${from?.firstName} " +
-            "${ if (isIncoming) "получил" else "отправил" } сообщение \"$text\""
+            "${ if (isIncoming) "получил" else "отправил" } сообщение \"$text\" ${date.humanizeDiff()}"
 }
